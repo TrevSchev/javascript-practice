92f803bcce8e6c69d1764339ab56aa21c63fc786
TrevSchev <schevingtrevor@gmail.com>
2022-09-22 18:25:01 -0700

    take-home branch/take-home challenge

--- js/script.js
+++ js/script.js
@@ -2,3 +2,36 @@
 
 
 alert("it works")
+
+// Case sensitivity - certain script requires the case 
+// Semicolons - acts as a period at the end of a sentence. recomended in best practices 
+
+console.log("hello there");
+
+console.log("another log that will show up in the console on the browser that you can find when you inspect a page");
+
+// Data Types (Categories) (Category Types)
+
+/* 
+PRIMITIVES:
+- Numbers - all numbers i.e. 5, 2.5, 0, -8, 1244.34, NaN, Infinity
+
+- Strings - text, word, sentences, think of a necklace that strings letters together to make a work.
+    express strings with any of the following 3 options:
+    -"" double quotes
+    -'' single quotes
+    -`` backticks
+
+- Boolean - true and false i.e. (yes and no) (on and off) (1 and 0)
+
+- Null - only has null itself (nothingness)
+
+- Undefined - like a placeholder to be discovered ex) like how we know aliens or something is out there, but we don't know what it is. It is undefined.
+
+- Symbol - a unique value that gets for you
+
+COMPLEX
+- array - a list of primitives
+- object - a structure that allows you to describe an "object" and describe the actions that the "object" can do.
+
+*/
